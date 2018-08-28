4ce55aaa887acc8f82646fea68c208b5c17fa38f
haakondf <haakondf@gmail.com>
2018-08-28 10:49:26 +0200

    collaberating

--- index.js
+++ index.js
@@ -5,10 +5,4 @@ console.log('A change');
 
 console.log('Hello World')
 
-var x = 2;
-
-var b = 5;
-
-var c = x + b;
-
-console.log(c);
+console.log('Hello Elizaveta, did I write the name correctly for once?')
