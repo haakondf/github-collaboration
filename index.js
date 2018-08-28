e1861d58119a8088cb1cfcd2056473fd09f842da
haakondf <haakondf@gmail.com>
2018-08-28 10:53:10 +0200

    v3

--- index.js
+++ index.js
@@ -6,3 +6,5 @@ console.log('A change');
 console.log('Hello World')
 
 console.log('Hello Elizaveta, did I write the name correctly for once?')
+
+console.log('Ja, meinen deutch bist sehr gut, danke')
